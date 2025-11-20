82bbb93cbb1952a77130ef3a00b4e69621f4e255
Leslie <lesliexjh@gmail.com>
2025-11-20 23:46:07 +0800

    Add files via upload

---
+++ python/05_Python函数/01_函数的定义.py
@@ -0,0 +1,11 @@
+'''
+def 函数名(传入参数):
+    函数体
+    return 返回值
+'''
+#定义一个函数
+def say_hi():
+    print("Leslie")
+
+#调用参数
+say_hi()
---
+++ python/05_Python函数/02_函数的参数.py
@@ -0,0 +1,6 @@
+def sum_xy(x,y):
+    sum=x+y
+    print(f'{x}+{y}={sum}')
+    return sum
+
+sum_xy(1,2)
