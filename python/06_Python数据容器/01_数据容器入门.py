5d22b489d5ba55ab2df81735bd56995cc907464d
Leslie <lesliexjh@gmail.com>
2025-11-23 22:07:41 +0800

    Add files via upload

--- python/06_Python数据容器/01_数据容器入门.py
+++ python/06_Python数据容器/01_数据容器入门.py
@@ -5,3 +5,27 @@
 集合
 字典
 """
+
+"""
+字面量
+[元素1，元素2，元素3，。。。]
+定义变量
+变量名称=[元素1，元素2，元素3，。。。]
+定义空列表
+变量名称=[]
+变量名称=list()
+"""
+my_list = [1, 2, 3]
+print(my_list)
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list[-1])
+print(my_list[-2])
+print(my_list[-3])
+
+my_list = [[1, 2, 3], [4, 5, 6]]
+print(my_list)
+print(type(my_list))
+print(my_list[0])
+print(my_list[0][1])
