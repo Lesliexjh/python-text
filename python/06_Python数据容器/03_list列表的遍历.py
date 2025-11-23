7b233294db2ab1e805ecbebc4da374a91262b330
Leslie <lesliexjh@gmail.com>
2025-11-24 00:48:50 +0800

    Add files via upload

---
+++ python/06_Python数据容器/03_list列表的遍历.py
@@ -0,0 +1,33 @@
+"""
+演示while循环和for循环的列表遍历
+"""
+
+
+def list_while_func():
+    """
+    使用while循环遍历列表的演示函数
+    :return: None
+    """
+    my_list = ['leslie','lucifer','fcxs']
+    #循环控制变量通过下标索引来控制，默认0
+    #每一次循环将下标索引变量+1
+    #循环条件：下标索引变量<列表的数量
+
+    #定义一个变量用来标记列表的下标
+    index = 0      #初始值为0
+    while index < len(my_list):
+        #通过index变量取出对应下标的元素
+        print(my_list[index])
+        index += 1
+list_while_func()
+
+
+def list_for_func():
+    my_list = ['leslie','lucifer','fcxs']
+    for index in range(len(my_list)):
+        print(my_list[index])
+
+    for x in my_list:
+        print(x)
+
+list_for_func()
